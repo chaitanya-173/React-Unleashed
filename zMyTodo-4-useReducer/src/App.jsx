2cc8869aa5fa500e81c5f1cdd43e1403b559e517
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2024-12-21 23:40:27 +0530

    commitment

---
+++ zMyTodo-4-useReducer/src/App.jsx
@@ -0,0 +1,67 @@
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import TodoItems from "./components/TodoItems";
+import WelcomeMessage from "./components/WelcomeMessage";
+import { useReducer } from "react";
+import { TodoItemsContext } from "./store/todo-items-store";
+
+const todoItemsReducer = (currTodoItems, action) => {
+  let newTodoItems = [];
+  if (action.type === "NEW_ITEM") {
+    newTodoItems = [
+      ...currTodoItems,
+      {
+        name: action.payload.itemName,
+        dueDate: action.payload.dueDate,
+      },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currTodoItems.filter((item) => item.name !== action.payload.itemName);
+  }
+  return newTodoItems;
+};
+
+function App() {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
+  const addNewItem = (itemName, dueDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        dueDate,
+      },
+    };
+    dispatchTodoItems(newItemAction);
+  };
+
+  const deleteItem = (itemName) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
+  };
+
+  return (
+    <TodoItemsContext.Provider
+      value={{
+        todoItems,
+        addNewItem,
+        deleteItem,
+      }}
+    >
+      <center className="todo-container">
+        <AppName />
+        <AddTodo />
+        <WelcomeMessage></WelcomeMessage>
+        <TodoItems></TodoItems>
+      </center>
+    </TodoItemsContext.Provider>
+  );
+}
+
+export default App;
